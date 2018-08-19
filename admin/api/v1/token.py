2cecaa5052334adade4660fcc9133e2acb3d0b7c
fengzhanyuan2016 <870831935@qq.com>
2018-08-19 08:27:47 +0800

    dd

--- admin/api/v1/token.py
+++ admin/api/v1/token.py
@@ -2,13 +2,14 @@ import json
 from flask import Blueprint, Response
 from admin.models.user import User
 
-token=Blueprint('token',__name__)
+token = Blueprint('token', __name__)
+
 
 @token.route("/token", methods=['POST'])
 def get_token():
     result = User.verify("feng", "123456")
     if result:
-        token=""
+        token = ""
     response = json.dumps({"data": token})
     return Response(response, mimetype="application/json")
     pass

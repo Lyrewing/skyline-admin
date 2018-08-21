a70b453a6a87e65e7e10fb7abde1efa95497240d
CHINA\QXU8000 <Zhanyuan.Feng@partner.bmw.com>
2018-08-21 10:23:03 +0800

    route

--- admin/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from admin.config import DevConfig
-
-app = Flask(__name__)
-# 设置环境配置
-app.config.from_object(DevConfig)
--- admin/app.py
+++ admin/app.py
@@ -1,12 +1,21 @@
-from admin import app
-from admin.routes import register_blueprints
 from admin.libs.error import ApiException
 from werkzeug.exceptions import HTTPException
+from flask import Flask
+from admin.config import DevConfig
+from admin.api.v1.book import book
+from admin.api.v1.user import user
+from admin.api.v1.token import token
 
+
+app = Flask(__name__)
+# 设置环境配置
+app.config.from_object(DevConfig)
 # 注册的路由
-register_blueprints()
+app.register_blueprint(user, url_prefix="/v1/user")
+app.register_blueprint(book, url_prefix="/v1/book")
+app.register_blueprint(token, url_prefix="/v1/token")
 
-
+#全局异常过滤器
 @app.errorhandler(Exception)
 def framwork_error(e) -> ApiException:
     if isinstance(e, ApiException):
@@ -18,6 +27,7 @@ def framwork_error(e) -> ApiException:
         return ApiException(msg, code, error_code)
     else:
         return ApiException()
+
 
 
 if __name__ == '__main__':

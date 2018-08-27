991270bdda8c38a06301a9f4acd5a4b2872b13e2
fengzhanyuan2016 <870831935@qq.com>
2018-08-27 20:52:55 +0800

    forms

--- tests.py
+++ tests.py
@@ -1,24 +1,3 @@
-from cmd import Cmd
-
-
 def test_add():
     assert 3 * 4 == 12
 
-
-class Cli(Cmd):
-    def __init__(self):
-        Cmd.__init__(self)
-
-    def do_hello(self, line):
-        print("hello" + line)
-
-
-def test_cmd():
-    cli = Cli()
-    try:
-        cli.cmdloop()
-    except:
-        exit()
-
-
-test_cmd()
